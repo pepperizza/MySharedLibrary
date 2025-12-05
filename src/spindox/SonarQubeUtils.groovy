0ced6dad1d2aed0159c9e1fad0b540965c8dc9b5
pepperizza <pepperre3@gmail.com>
2025-12-05 12:55:41 +0100

    Update SonarQubeUtils.groovy

--- src/spindox/SonarQubeUtils.groovy
+++ src/spindox/SonarQubeUtils.groovy
@@ -13,29 +13,32 @@ class SonarQubeUtils implements Serializable {
      * @param repoName = nome del repository GitHub
      */
     def scanProject(String repoName) {
+        withCredentials([string(credentialsId: 'b47e8268-8ed2-458f-8078-5c3a9b8420b2', variable: 'SONAR_TOKEN')]) {
+            
+            steps.echo "=== Avvio scansione Sonar per repo: ${repoName} ==="
+    
+            steps.stage("Checkout") {
+                steps.echo "DEBUG: URL = https://github.com/pepperizza/${repoName}.git"
+                steps.git(
+                    url: "https://github.com/pepperizza/${repoName}.git",
+                    branch: "main"
+                )
+            }
+    
+            withSonarQubeEnv('sonarqube') {
 
-        steps.echo "=== Avvio scansione Sonar per repo: ${repoName} ==="
-
-        steps.stage("Checkout") {
-            steps.echo "DEBUG: URL = https://github.com/pepperizza/${repoName}.git"
-            steps.git(
-                url: "https://github.com/pepperizza/${repoName}.git",
-                branch: "main"
-            )
+                    bat """
+                    "C:\\SonarScanner\\sonar-scanner-cli-7.3.0.5189-windows-x64\\bin\\sonar-scanner.bat" ^
+                      -Dsonar.projectKey=${repoName} ^
+                      -Dsonar.projectName="${repoName}" ^
+                      -Dsonar.sources=. ^
+                      -Dsonar.host.url=%SONAR_HOST_URL% ^
+                      -Dsonar.token=%SONAR_TOKEN%
+                    """
+                }
+    
+            steps.echo "=== Scansione completata per ${repoName} ==="
         }
-
-        steps.stage("Sonar Scanner") {
-          
-          steps.bat """
-              sonar-scanner.bat ^
-              -Dsonar.projectKey=${repoName} ^
-              -Dsonar.sources=. ^
-              -Dsonar.host.url=%SONAR_HOST_URL% ^
-              -Dsonar.login=%SONAR_TOKEN%
-          """
-        }
-
-        steps.echo "=== Scansione completata per ${repoName} ==="
     }
 }
 
